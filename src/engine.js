78419a3cb3b015f111a831d1ba1e2fb0012f9c33
Luís Pinto <fc60831@alunos.fc.ul.pt>
2026-07-20 16:55:26 +0100

    removed fetch (no data collected)

--- src/engine.js
+++ src/engine.js
@@ -19,37 +19,37 @@ let performance = {
     elapsed: 0,
     user_id: null,
     observations: null,
-}
+};
 
 let sequence = {
     coordinates: [],
     position: undefined,
 };
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
     completeHtmlInformation();
     makeGrid();
     setupEvents();
 });
 
 function completeHtmlInformation() {
-    document.getElementById('sequence-length').innerHTML = specs.sequenceLength;
-    document.getElementById('grid-cols').innerHTML = specs.width;
-    document.getElementById('grid-rows').innerHTML = specs.height;
+    document.getElementById("sequence-length").innerHTML = specs.sequenceLength;
+    document.getElementById("grid-cols").innerHTML = specs.width;
+    document.getElementById("grid-rows").innerHTML = specs.height;
 }
 
 function makeGrid() {
-    let grid = document.getElementById('grid');
+    let grid = document.getElementById("grid");
 
     for (let x = 0; x < specs.width; x++) {
         for (let y = 0; y < specs.height; y++) {
-            let cell = document.createElement('span');
-            cell.classList.add('cell');
+            let cell = document.createElement("span");
+            cell.classList.add("cell");
             cell.id = `cell-${x}-${y}`;
             grid.appendChild(cell);
 
-            let target = document.createElement('span');
-            target.classList.add('target');
+            let target = document.createElement("span");
+            target.classList.add("target");
             target.id = `target-${x}-${y}`;
             cell.appendChild(target);
         }
@@ -62,9 +62,11 @@ function makeGrid() {
 }
 
 function setupEvents() {
-    document.querySelector('#login form').addEventListener('submit', login);
-    document.querySelector('#grid').addEventListener('click', gridClick);
-    document.querySelector('#results button').addEventListener('click', newSequence);
+    document.querySelector("#login form").addEventListener("submit", login);
+    document.querySelector("#grid").addEventListener("click", gridClick);
+    document
+        .querySelector("#results button")
+        .addEventListener("click", newSequence);
 }
 
 function login(evt) {
@@ -74,21 +76,24 @@ function login(evt) {
 }
 
 function newSequence() {
-    setupSequence().then(() => {
-        startTime = Date.now();
+    setupSequence()
+        .then(() => {
+            startTime = Date.now();
 
-        document.getElementById('login').style.display = 'none';
-        document.getElementById('results').style.display = 'none';
-        document.getElementById('arena').style.display = 'block';
-    }).catch(errors => {
-        if (Array.isArray(errors)) {
-            alert(errors.join('\n'));
-        } else {
-            alert(errors);
-        }
-    });
+            document.getElementById("login").style.display = "none";
+            document.getElementById("results").style.display = "none";
+            document.getElementById("arena").style.display = "block";
+        })
+        .catch((errors) => {
+            if (Array.isArray(errors)) {
+                alert(errors.join("\n"));
+            } else {
+                alert(errors);
+            }
+        });
 }
 
+/*
 async function getSequence() {
     if (location.protocol == 'file:') {
         return Promise.resolve(getLocalSequence());
@@ -114,6 +119,7 @@ async function getSequence() {
         throw result.errors;
     }
 }
+    */
 
 function getLocalSequence() {
     let coordinates = [];
@@ -127,7 +133,7 @@ function getLocalSequence() {
 
     return {
         coordinates,
-        hash: undefined
+        hash: undefined,
     };
 }
 
@@ -150,24 +156,25 @@ async function setupSequence() {
 
 function advanceSequence() {
     // Remove the goal-n classes from all targets
-    for (const el of document.getElementsByClassName('target')) {
-        el.className = 'target';
+    for (const el of document.getElementsByClassName("target")) {
+        el.className = "target";
     }
 
     // Advance inside the sequence
     sequence.position++;
-    document.getElementById('position').innerHTML = sequence.position;
+    document.getElementById("position").innerHTML = sequence.position;
 
     // If we are at the end of the sequence, stop collecting data
     if (sequence.position == sequence.coordinates.length) {
         stopSequence();
-        return
+        return;
     }
 
     // Otherwise, annotate the new targets with the goal-n classes
     for (
         let i = 0;
-        i < specs.peekables && sequence.position + i < sequence.coordinates.length;
+        i < specs.peekables &&
+        sequence.position + i < sequence.coordinates.length;
         i++
     ) {
         let [x, y] = sequence.coordinates[sequence.position + i];
@@ -180,36 +187,42 @@ function stopSequence() {
 
     sendResults();
 
-    document.getElementById('arena').style.display = 'none';
+    document.getElementById("arena").style.display = "none";
 
     showResults();
 
-    if (typeof processEnd == 'function') {
+    if (typeof processEnd == "function") {
         processEnd(performance);
     }
 }
 
 function showResults() {
-    document.getElementById('results').style.display = 'block';
+    document.getElementById("results").style.display = "block";
 
-    document.getElementById('results-sent').style.display =
-        performance.hash ? 'block' : 'none';
+    document.getElementById("results-sent").style.display = performance.hash
+        ? "block"
+        : "none";
 
-    document.querySelector('#results-age').innerHTML = performance.age;
-    document.querySelector('#results-successes').innerHTML = performance.successes;
-    document.querySelector('#results-failures').innerHTML = performance.failures;
-    document.querySelector('#results-mistakes').innerHTML = performance.mistakes;
-    document.querySelector('#results-time').innerHTML = Math.floor(performance.elapsed);
+    document.querySelector("#results-age").innerHTML = performance.age;
+    document.querySelector("#results-successes").innerHTML =
+        performance.successes;
+    document.querySelector("#results-failures").innerHTML =
+        performance.failures;
+    document.querySelector("#results-mistakes").innerHTML =
+        performance.mistakes;
+    document.querySelector("#results-time").innerHTML = Math.floor(
+        performance.elapsed,
+    );
 }
 
 function sendResults() {
     if (performance.hash) {
         fetch(
-            'http://areasgrupo.alunos.di.fc.ul.pt/~ipm000/projb/store_results.php',
+            "http://areasgrupo.alunos.di.fc.ul.pt/~ipm000/projb/store_results.php",
             {
-                method: 'POST',
+                method: "POST",
                 body: JSON.stringify(performance),
-            }
+            },
         );
     }
 }
@@ -217,11 +230,11 @@ function sendResults() {
 function gridClick(evt) {
     let target = evt.target;
 
-    let grid = document.getElementById('grid');
-    let xGrid = grid.getBoundingClientRect().x
-    let yGrid = grid.getBoundingClientRect().y
+    let grid = document.getElementById("grid");
+    let xGrid = grid.getBoundingClientRect().x;
+    let yGrid = grid.getBoundingClientRect().y;
 
-    let goal = document.querySelector('.goal-0');
+    let goal = document.querySelector(".goal-0");
     let xGoal = goal.getBoundingClientRect().x - xGrid;
     let yGoal = goal.getBoundingClientRect().y - yGrid;
 
@@ -231,25 +244,25 @@ function gridClick(evt) {
     let interaction = {
         x: x,
         y: y,
-        distance: Math.sqrt((xGoal - x) * (xGoal - x) + (yGoal - y) * (yGoal - y)),
+        distance: Math.sqrt(
+            (xGoal - x) * (xGoal - x) + (yGoal - y) * (yGoal - y),
+        ),
         elapsed: (Date.now() - prevClick) / 1000,
     };
 
-    const onTarget = target.id.startsWith('target');
+    const onTarget = target.id.startsWith("target");
 
     if (!onTarget) {
         performance.mistakes++;
-        interaction.type = 'mistake';
+        interaction.type = "mistake";
         interaction.class = null;
-    }
-    else if (target.classList.contains('goal-0')) {
+    } else if (target.classList.contains("goal-0")) {
         performance.successes++;
-        interaction.type = 'success';
+        interaction.type = "success";
         interaction.class = getClassName(target);
-    }
-    else {
+    } else {
         performance.failures++;
-        interaction.type = 'failure';
+        interaction.type = "failure";
         interaction.class = getClassName(target);
     }
 
@@ -261,7 +274,7 @@ function gridClick(evt) {
         advanceSequence();
     }
 
-    if (typeof processClick === 'function') {
+    if (typeof processClick === "function") {
         interaction.target = target;
         processClick(interaction);
         delete interaction.target;
@@ -269,29 +282,33 @@ function gridClick(evt) {
 }
 
 function getClassName(el) {
-    return Array.from(el.classList).filter(x => x !== 'target').join(' ');
+    return Array.from(el.classList)
+        .filter((x) => x !== "target")
+        .join(" ");
 }
 
 function validateLogin() {
-    let age = document.querySelector('#login input').value;
+    let age = document.querySelector("#login input").value;
 
     if (!age.match(/^\d+$/)) {
-        document.getElementById('error-age').innerText = 'Wrong age format: age must consist of digits only (0-9)';
-        document.getElementById('error-age').style.display = 'block';
+        document.getElementById("error-age").innerText =
+            "Wrong age format: age must consist of digits only (0-9)";
+        document.getElementById("error-age").style.display = "block";
         return false;
     }
 
     performance.age = parseInt(age);
 
     if (performance.age <= 6) {
-        document.getElementById('error-age').innerText = 'You must be at least 7 to play!';
-        document.getElementById('error-age').style.display = 'block';
+        document.getElementById("error-age").innerText =
+            "You must be at least 7 to play!";
+        document.getElementById("error-age").style.display = "block";
         return false;
     }
 
     performance.user_id = getUserId();
 
-    if (typeof processStart == 'function') {
+    if (typeof processStart == "function") {
         processStart(performance);
     }
 
@@ -309,14 +326,15 @@ function getUserId() {
 function generateUserId() {
     const length = 16;
 
-    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     let result = [];
 
     for (let i = 0; i < length; i++) {
         result.push(
-            characters.charAt(Math.floor(Math.random() * characters.length))
+            characters.charAt(Math.floor(Math.random() * characters.length)),
         );
     }
 
-    return result.join('');
+    return result.join("");
 }
